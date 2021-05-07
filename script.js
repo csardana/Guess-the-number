7ee1124ccd18bc6a945ea1115666576d0f604059
Chirag Sardana <csardana18@gmail.com>
2021-05-07 12:25:02 -0400

    Final Game

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // document.querySelector(".number").textContent = 13;
 // document.querySelector(".score").textContent = 10;
 
-const rand = Math.trunc(20 * Math.random());
+var rand = Math.trunc(20 * Math.random()) + 1;
 console.log(rand);
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -19,18 +19,38 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === rand) {
     document.querySelector(".win").textContent = "Good Job Buddy 🤩";
     document.querySelector(".message").textContent = "Correct Number 🥳";
+    document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".highscore").textContent =
       Number(document.querySelector(".highscore").textContent) >
       Number(document.querySelector(".score").textContent)
         ? document.querySelector(".highscore").textContent
         : document.querySelector(".score").textContent;
   } else if (guess < rand) {
-    document.querySelector(".score").textContent =
-      Number(document.querySelector(".score").textContent) - 1;
-    document.querySelector(".message").textContent = "Too Low";
+    if (Number(document.querySelector(".score").textContent > 1)) {
+      document.querySelector(".score").textContent =
+        Number(document.querySelector(".score").textContent) - 1;
+      document.querySelector(".message").textContent = "Too Low";
+    } else {
+      document.querySelector(".message").textContent =
+        "Game Over , Press the again button to restart";
+    }
   } else if (guess > rand) {
-    document.querySelector(".score").textContent =
-      Number(document.querySelector(".score").textContent) - 1;
-    document.querySelector(".message").textContent = "Too High";
+    if (Number(document.querySelector(".score").textContent > 1)) {
+      document.querySelector(".score").textContent =
+        Number(document.querySelector(".score").textContent) - 1;
+      document.querySelector(".message").textContent = "Too High";
+    } else {
+      document.querySelector(".message").textContent =
+        "Game Over , Press the again button to restart";
+    }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = "20";
+  document.querySelector(".highscore").textContent = "0";
+  document.querySelector(".win").textContent = "";
+  document.querySelector(".guess").textContent = "";
+  rand = Math.trunc(20 * Math.random()) + 1;
 });
